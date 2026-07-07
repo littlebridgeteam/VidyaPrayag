93d423447f0f3efa0a5d3230b431f4b9001660ca
hereugo-ak <hereugo595@gmail.com>
2026-07-07 14:02:11 +0530

    Fix missing RoundedCornerShape import in ScheduledMessagesScreenV2

--- composeApp/src/commonMain/kotlin/com/littlebridge/enrollplus/ui/v2/screens/school/ScheduledMessagesScreenV2.kt
+++ composeApp/src/commonMain/kotlin/com/littlebridge/enrollplus/ui/v2/screens/school/ScheduledMessagesScreenV2.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
